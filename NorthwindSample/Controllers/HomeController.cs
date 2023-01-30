141beb918cb1ba29bb018e7b099d194e398b0fa7
Bayram Eren <bayram.eren@outlook.com.tr>
2023-01-30 10:52:48 +0300

    homecontrol

--- NorthwindSample/Controllers/HomeController.cs
+++ NorthwindSample/Controllers/HomeController.cs
@@ -1,38 +1,38 @@
-﻿//using LinqKit;
-//using Microsoft.AspNetCore.Mvc;
-//using NorthwindSample.Condition;
-//using NorthwindSample.Extensions;
-//using NorthwindSample.Models;
+﻿using LinqKit;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindSample.Condition;
+using NorthwindSample.Extensions;
+using NorthwindSample.Models;
 
-//namespace NorthwindSample.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class HomeController : ControllerBase
-//    {
-//        private readonly NorthwindContext _context;
+namespace NorthwindSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
 
-//        public HomeController(NorthwindContext context)
-//        {
-//            _context = context;
-//        }
+        public HomeController(NorthwindContext context)
+        {
+            _context = context;
+        }
 
-//        [HttpPost]
-//        public async Task<IActionResult> Get(Search search)
-//        {
-//            var expression = search.Where.GetLambdaExpression<Product>();
+        [HttpPost("Product")]
+        public async Task<IActionResult> Get(Search search)
+        {
+            var expression = search.Where.GetLambdaExpression<Product>();
 
-//            var products = await _context.Products.Where(expression).ToPagedListAsync(1, 10);
-//            return Ok(products);
-//        }
+            var products = await _context.Products.Where(expression).ToPagedListAsync(1, 10);
+            return Ok(products);
+        }
 
-//        [HttpPost("Order")]
-//        public async Task<IActionResult> GetOrder(Search search)
-//        {
-//            var expression = search.Where.GetLambdaExpression<Order>();
+        [HttpPost("Order")]
+        public async Task<IActionResult> GetOrder(Search search)
+        {
+            var expression = search.Where.GetLambdaExpression<Order>();
 
-//            var products = await _context.Orders.Where(expression).ToPagedListAsync(1, 10);
-//            return Ok(products);
-//        }
-//    }
-//}
+            var products = await _context.Orders.Where(expression).ToPagedListAsync(1, 10);
+            return Ok(products);
+        }
+    }
+}
